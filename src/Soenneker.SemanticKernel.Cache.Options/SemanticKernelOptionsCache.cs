8e756e595990c8285e11cdbb80140b4043e4025f
soenneker <jake@soenneker.com>
2026-09-03 22:01:44 -0500

    Add interface inheritdoc references

--- src/Soenneker.SemanticKernel.Cache.Options/SemanticKernelOptionsCache.cs
+++ src/Soenneker.SemanticKernel.Cache.Options/SemanticKernelOptionsCache.cs
@@ -8,6 +8,7 @@ using Soenneker.Dictionaries.Singletons;
 
 namespace Soenneker.SemanticKernel.Cache.Options;
 
+/// <inheritdoc cref="ISemanticKernelOptionsCache" />
 public sealed class SemanticKernelOptionsCache : ISemanticKernelOptionsCache
 {
     private readonly SingletonDictionary<SemanticKernelOptions, Func<ValueTask<SemanticKernelOptions>>> _options;
